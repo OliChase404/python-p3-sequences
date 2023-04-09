5835ec693c7fbd50b217861e7708bc71a3b6d1be
Oliver <olichase91@gmail.com>
2023-04-09 15:32:01 -0700

    Solved Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if(length == 0):
+        print([])
+    elif(length == 1):
+        print([0])
+    elif(length > 1):
+        responce = [0,1]
+        while(len(responce) < length):
+            next_number = responce[-2] + responce[-1]
+            responce.append(next_number)
+        print(responce)
+            
+print_fibonacci(1)
